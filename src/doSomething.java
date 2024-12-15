7b512073e81468b5fdda410c98ac66382624da31
rajeshPoddar26 <rajeshkumarpoddar26@gmail.com>
2024-12-15 10:50:58 -0800

    first commit

---
+++ src/doSomething.java
@@ -0,0 +1,12 @@
+public class doSomething {
+
+
+    public static void doSomething(){
+        System.out.print("Hi friend");
+    }
+
+    public static void main(String[] args) {
+        doSomething();
+
+    }
+}
